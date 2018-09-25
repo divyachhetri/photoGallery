0fa78400eaab370393bc8fce1223fc634a5251e2
Divya Pandit Chhetri <divya@divyas-air.fios-router.home>
2018-09-25 19:56:13 -0400

    Initial Commit

---
+++ photoGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photoGallery
+//
+//  Created by Divya Pandit Chhetri on 9/25/18.
+//  Copyright © 2018 Divya Pandit Chhetri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
